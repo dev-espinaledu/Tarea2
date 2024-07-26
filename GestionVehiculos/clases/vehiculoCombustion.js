6f6dfed38ab26d5cc6a2cefd02e42ab245c7140a
dev-espinaledu <espinalsolarte7@gmail.com>
2024-07-26 14:49:35 -0500

    cambios

--- GestionVehiculos/clases/vehiculoCombustion.js
+++ GestionVehiculos/clases/vehiculoCombustion.js
@@ -1,14 +1,14 @@
 class VehiculoCombustion {
-  constructor(nivelCombustible) {
-      this.nivelCombustible = nivelCombustible;
-  }
-  llenarTanque(carga) {
-      this.nivelCombustible + carga;
-      console.log("Nivel de combustible después de llenar: " + this.nivelCombustible + "%");
-  }
-  usarCombustible(uso) {
-      this.nivelCombustible - uso;
-      console.log("Nivel de combustible después de usar: " + this.nivelCombustible + "%");
-  }
+    constructor(nivelCombustible) {
+        this.nivelCombustible = nivelCombustible;
+    }
+    llenarTanque(carga) {
+        this.nivelCombustible + carga;
+        console.log("Nivel de combustible después de llenar: " + this.nivelCombustible + "%");
+    }
+    usarCombustible(uso) {
+        this.nivelCombustible - uso;
+        console.log("Nivel de combustible después de usar: " + this.nivelCombustible + "%");
+    }
 }
 export default VehiculoCombustion;
